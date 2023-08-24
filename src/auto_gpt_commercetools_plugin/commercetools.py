b6a1a8bd74e3986ba8256710d3537acf283c134a
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-08-24 15:13:15 -0400

    fix step

--- src/auto_gpt_commercetools_plugin/commercetools.py
+++ src/auto_gpt_commercetools_plugin/commercetools.py
@@ -1,10 +1,5 @@
-import commercetools
 from commercetools.platform.client import Client
 from commercetools.platform.models.graph_ql import GraphQLRequest
-from autogpt.processing.text import chunk_content, split_text, summarize_text
-from autogpt.config import ConfigBuilder
-import json
-from autogpt.logs import logger
 
 import os
 
@@ -17,6 +12,7 @@ class CT:
         self.project_key = os.environ["CTP_PROJECT_KEY"]
         self.client_id = os.environ["CTP_CLIENT_ID"]
         self.client_secret = os.environ["CTP_CLIENT_SECRET"]
+        self.gen = None
 
         # SDK settings
         self.auth_url = os.environ["CTP_AUTH_URL"]
@@ -31,7 +27,6 @@ class CT:
                 token_url=self.auth_url,
             )
 
-        self.gen = None
 
     def ct_execute_graph_ql(self, query):
 
@@ -64,11 +59,8 @@ class CT:
         result = self.client.with_project_key(project_key=self.project_key).graphql().post(body=body)
         return result
 
-    def get_one_order (self):
-        logger.info('Running get one order ****', str(self.offset))
-        
+    def get_one_order (self):        
         while True:   
-            logger.info('In while true')
             n = self.offset
             query = f"""
                 query {{
@@ -94,9 +86,7 @@ class CT:
             self.offset += 1
 
     def ct_get_all_orders(self):
-        logger.info('----run')
         if (self.count < 0):
-            logger.info('count -1')
             query = """
                 query {
                     orders {
@@ -108,13 +98,13 @@ class CT:
 
             result = self.client.with_project_key(project_key=self.project_key).graphql().post(body=body)
             self.count = result.data['orders']['count']
-            logger.info('count is' + str(self.count))
             if (self.count > 0):
-                logger.info('setting gen')
                 self.gen = self.get_one_order()
 
         if (self.count > 0):
-            logger.info('On else >> '+ str(self.count))
-            return next(self.gen)
+            is_there_more = '\n Go to next step '
+            if (self.count - 1 > self.offset):
+                is_there_more = "\n Get the next order "
+            return next(self.gen) + is_there_more
 
         return ''
